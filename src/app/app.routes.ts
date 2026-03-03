189944fc8cb478bdafe2d361bd48679212481d04
sivaprakasamSK <sivaprakasams267@gmail.com>
2026-03-03 11:08:02 +0530

    updated the app modules

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { Spoc } from './spoc/spoc';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'spoc',component:Spoc}
+];
